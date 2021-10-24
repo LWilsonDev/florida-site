8e34d04dc29de39add63b534b2e0b23c65f3840c
Lucy Wilson <33698364+LWilsonDev@users.noreply.github.com>
2021-10-24 12:21:00 +0100

    remove images

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,12 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/gatsby-config.js`,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/static/img`,
